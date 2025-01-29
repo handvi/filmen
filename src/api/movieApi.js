a3d006ae4655341eb800e27708df2cc2060ab716
handvi <faisalhanafi98.jobs@gmail.com>
2025-01-30 01:12:34 +0700

    fix api

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -6,7 +6,7 @@ const categories = ['series', 'action', 'movie', 'horror'];
 
 export const fetchMoviesByCategory = async (category) => {
   try {
-    const response = await axios.get(`http://www.omdbapi.com/?s=${category}&apikey=${apiKey}`);
+    const response = await axios.get(`https://www.omdbapi.com/?s=${category}&apikey=${apiKey}`);
     return response.data.Search || [];
   } catch (error) {
     console.error('Error fetching data: ', error);
